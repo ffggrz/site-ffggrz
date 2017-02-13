2c90a2dc7de5005751ee0c64b8b3bb3aaba88bc3
Sunz3r <Sunz3r@users.noreply.github.com>
2017-02-13 21:54:31 +0100

    insert fastd-modules

--- site.mk
+++ site.mk
@@ -14,16 +14,18 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
-	gluon-config-mode-tunneldigger \
+	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
+	gluon-luci-mesh-vpn-fastd \
 	gluon-luci-autoupdater \
 	gluon-luci-node-role \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
 	gluon-neighbour-info \
+	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
