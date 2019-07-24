b8daf64b6b0408466b338a516f81467f45e7b40d
anoy <anoymouserver@users.noreply.github.com>
2019-07-24 07:59:41 +0200

    removed gluon-ebtables-limit-arp since its included by default
    - see https://gluon.readthedocs.io/en/latest/releases/v2018.2.html#gluon-ebtables-limit-arp-enabled-by-default (1a690d2)

--- site.mk
+++ site.mk
@@ -9,7 +9,6 @@ GLUON_FEATURES := \
 	autoupdater \
 	ebtables-filter-multicast \
 	ebtables-filter-ra-dhcp \
-	ebtables-limit-arp \
 	mesh-batman-adv-15 \
 	mesh-vpn-fastd \
 	mesh-vpn-tunneldigger \
