1d64afb7de89d08a93643a42a900a83e0d8d567b
Jörg Deckert <joergd@bitquell.de>
2023-06-08 10:50:05 +0200

    Ampassungen an Gluon v2022.1.4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # site-ffggrz
 
-## Für Gera-Greiz Firmware Builds genutzte Gluon Versionen:
+## Für Gera-Greiz Firmware Builds genutzte Branches:
 
 - __stable:__ site configuration for stable firmware builds
 - __beta:__ site configuration for beta firmware builds
@@ -9,3 +9,27 @@
 [Downloads](https://github.com/ffggrz/site-ffggrz/releases) der Site-Configs.
 
 Weitere Infos zur Site sind in der [Gluon-Doku](http://gluon.readthedocs.org/) unter 'Site' zu finden.
+
+Ab den Firmware-Versionen 1.4.0 bzw. 20230605-exp nutzen wir ein Standard-Gluon ohne eigene Anpassungen. Um eine Firmware für die Freifunk-Community Gera-Greiz zu erstellen, wird also das passende offizielle Gluon-Release (im folg. Beispiel v2022.1.4) ausgecheckt:
+```
+export RELEASE="v2022.1.4"
+git clone https://github.com/freifunk-gluon/gluon.git gluon-$RELEASE -b $RELEASE
+cd gluon-$RELEASE
+```
+
+Nun wird die zum zu erstellenden Release (im folg. Beispiel 1.4.0) der Freifunk-Community Gera-Greiz passende Site-Konfiguration aus diesem Repository ausgecheckt:
+```
+export GLUON_RELEASE="1.4.0"
+git clone https://github.com/ffggrz/site-ffggrz.git site -b $GLUON_RELEASE
+```
+
+Anschließend wird die Firmware entsprechend der o.g. Guon-Doku erstellt (im Beispiel für den stable Branch):
+```
+export GLUON_LANGS="en de"
+export GLUON_AUTOUPDATER_BRANCH=stable
+export GLUON_AUTOUPDATER_ENABLED=1
+make update
+make -j4 GLUON_TARGET=ath79-generic
+```
+
+Im Beispiel wird die Firmware für alle Router des Targets "ath79-generic" erzeugt. Die möglichen targets kann man sich mit ```make list-targets``` anzeigen lassen.
--- site.mk
+++ site.mk
@@ -1,71 +1,37 @@
-##	gluon site.mk makefile example
-
-##	GLUON_FEATURES
-#		Specify Gluon features/packages to enable;
-#		Gluon will automatically enable a set of packages
-#		depending on the combination of features listed
-
 GLUON_FEATURES := \
-	autoupdater \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	ebtables-source-filter \
-	mesh-batman-adv-15 \
-	mesh-vpn-fastd \
-	mesh-vpn-tunneldigger \
-	radv-filterd \
-	respondd \
-	status-page \
-	web-mesh-vpn-fastd \
-	web-private-wifi \
-	web-advanced \
-	web-wizard
+  autoupdater \
+  ebtables-filter-multicast \
+  ebtables-filter-ra-dhcp \
+  ebtables-limit-arp \
+  ebtables-source-filter \
+  mesh-batman-adv-15 \
+  mesh-vpn-fastd \
+  mesh-vpn-fastd-l2tp \
+  radv-filterd \
+  respondd \
+  status-page \
+  web-mesh-vpn-fastd \
+  web-private-wifi \
+  web-advanced \
+  web-wizard
 
-##	GLUON_SITE_PACKAGES
-#		Specify additional Gluon/OpenWrt packages to include here;
-#		A minus sign may be prepended to remove a packages from the
-#		selection that would be enabled by default or due to the
-#		chosen feature flags
+GLUON_FEATURES_standard := \
+  config-mode-geo-location-osm \
+  wireless-encryption-wpa3
 
-GLUON_SITE_PACKAGES := \
-	iwinfo
-
-##	GLUON_SITE_PACKAGES_standard
-#		Same as GLUON_SITE_PACKAGES but only for devices of the standard device-class
+GLUON_SITE_PACKAGES := iwinfo
 
 GLUON_SITE_PACKAGES_standard := \
-	respondd-module-airtime
+  respondd-module-airtime
 
-##	DEFAULT_GLUON_RELEASE
-#		version string to use for images
-#		gluon relies on
-#			opkg compare-versions "$1" '>>' "$2"
-#		to decide if a version is newer or not.
-
-DEFAULT_GLUON_RELEASE := $(shell date '+%Y%m%d')-exp
-
-# Variables set with ?= can be overwritten from the command line
-
-##	GLUON_RELEASE
-#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
-#		e.g.:
-#			$ make images GLUON_RELEASE=23.42+5
-#		would generate images named like this:
-#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-# Default priority for updates.
 GLUON_PRIORITY ?= 0
 
-# Region code required for some images; supported values: us eu
 GLUON_REGION ?= eu
 
-# Languages to include
 GLUON_LANGS ?= en de
 
-# Build only sysupgrade images for deprecated devices
 GLUON_DEPRECATED ?= upgrade
-
-# Build images also for devices marked broken
-BROKEN ?= 1
